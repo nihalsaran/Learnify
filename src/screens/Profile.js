da131296bc63e45ce3cc92b40fc3486b0691e84b
Nihal saran das duggirala <92871743+nihalsaran@users.noreply.github.com>
2023-12-28 21:13:33 +0530

    update profile

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -45,12 +45,12 @@ const ProfilePage = () => {
     <View style={styles.container}>
       {userData ? (
         <View style={styles.profileInfo}>
-          <Text style={styles.infoText}>Name: {userData.name}</Text>
-          <Text style={styles.infoText}>Email: {userData.email}</Text>
-          <Text style={styles.infoText}>Roll Number: {userData.rollNumber}</Text>
-          <Text style={styles.infoText}>Selected Faculty: {userData.selectedFaculty}</Text>
-          <Text style={styles.infoText}>Selected Semester: {userData.selectedSemester}</Text>
-          <Text style={styles.infoText}>
+          <Text style={styles.infoText1}>Name: {userData.name}</Text>
+          <Text style={styles.infoText2}>Email: {userData.email}</Text>
+          <Text style={styles.infoText3}>Roll Number: {userData.rollNumber}</Text>
+          <Text style={styles.infoText4}>Selected Faculty: {userData.selectedFaculty}</Text>
+          <Text style={styles.infoText5}>Selected Semester: {userData.selectedSemester}</Text>
+          <Text style={styles.infoText6}>
             Total Subjects: {userData.subjects && userData.subjects.length > 0 ? userData.subjects[0].totalSubjects : 'Not available'}
           </Text>
         </View>
@@ -71,13 +71,41 @@ const styles = StyleSheet.create({
     bottom: 200,
   },
   profileInfo: {
-    backgroundColor: '#f0f0f0',
+    
     padding: 20,
     borderRadius: 8,
+    height: '50%',
+    width: '100%',
   },
-  infoText: {
+  infoText1: {
+    fontSize: 16,
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+  infoText2: {
+    fontSize: 16,
+    textAlign: 'center',
+    marginBottom: 12,
+  },
+  infoText3: {
+    fontSize: 16,
+    textAlign: 'center',
+    marginBottom: 14,
+  },
+  infoText4: {
+    fontSize: 16,
+    textAlign: 'center',
+    marginBottom: 16,
+  },
+  infoText5: {
+    fontSize: 16,
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+  infoText6: {
     fontSize: 16,
     marginBottom: 8,
+    textAlign: 'center',
   },
   loadingText: {
     fontSize: 18,
